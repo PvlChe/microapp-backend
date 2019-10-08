af40cfb575ae6164a1329897096ece908b1ac2ff
pavel.chachotkin <pavel.chachotkin@form4.de>
2019-10-08 18:48:30 +0200

    order

--- item/controller/item.js
+++ item/controller/item.js
@@ -45,3 +45,30 @@ exports.getItem = (req, res, next) => {
     });
     next();
 };
+
+exports.updateItem = (req, res, next) => {
+    console.log('test get item id');
+    const id = req.params.id;
+    return Item.findOneAndUpdate({_id: id}, function (err, item) {
+        item.amount = item.amount - req.body.amount;
+        doc.save();
+    }).then(data => {
+        //res.status(200).json(data);
+        console.log('test get data', data);
+        if (!data || data.length === 0) {
+            const error = new Error('Item couldnt be found!');
+            error.statusCode = 401;
+            throw error;
+        }
+        res.status(200);
+        res.locals.items = data;
+        res.locals.processed = true;
+        next();
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+    next();
+};
